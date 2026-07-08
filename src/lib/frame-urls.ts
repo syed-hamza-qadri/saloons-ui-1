16cb5c3d3d4e2c1d5aa945c694ed71015b336a56
Syed Hamza Qadri <qadrihamza871@gmail.com>
2026-07-08 17:46:02 +0500

    Revert "changes"
    This reverts commit ec2acb9bdbb7f8bb0562deb212df8066321fd059.

--- src/lib/frame-urls.ts
+++ src/lib/frame-urls.ts
@@ -1,3 +1,3 @@
-export const FRAME_URLS = Array.from({ length: 80 }, (_, i) =>
-  `/frames/frame_${String(i).padStart(4, "0")}.webp`
+export const FRAME_URLS = Array.from({ length: 240 }, (_, i) =>
+  `/frames/frame_${String(i).padStart(4, "0")}.jpg`
 );
